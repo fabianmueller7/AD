5885c25adfef23af3f416b4e2182ba0797f720b0
muellefa <muellerfabian@bluemail.ch>
2025-03-11 11:36:51 +0100

    E4 aufg 1

---
+++ aufgaben/src/main/java/e4/simpleHashset/CHashset.java
@@ -0,0 +1,86 @@
+package e4.simpleHashset;
+
+import java.util.ArrayList;
+
+public class CHashset {
+
+    private Element[] hashset;
+
+    public static void main(String[] args) {
+        CHashset hashset = new CHashset();
+        hashset.add(1);
+        hashset.add(3);
+        hashset.add(5);
+        hashset.add(7);
+
+
+        System.out.println(hashset);
+    }
+
+    public CHashset() {
+        this.hashset = new Element[10];
+    }
+
+    public void add(final int value) {
+
+            Element element = new Element(this.hash(value),value);
+            int prefferedIndex = this.hashCodeToArrayIndex(element.getHash());
+            int index = this.findEmptySlot(prefferedIndex);
+
+            this.hashset[index] = element;
+    }
+
+    public int getIndexOf(final int hashcode) {
+        int start = this.hashCodeToArrayIndex(hashcode);
+
+        while(this.getByIndex(start) != null) {
+            if(this.getByIndex(start).getHash() == hashcode) {
+                return start;
+            }
+            start++;
+        }
+        return -1;
+    }
+
+    public int size() {
+        return hashset.length;
+    }
+
+    public Element getByIndex(final int index) {
+        return hashset[index];
+    }
+
+    private int hashCodeToArrayIndex(final int hashCode) {
+        return hashCode % this.hashset.length;
+    }
+
+    private int hash(final int value){
+        return (value + 3) * 31;
+    }
+
+    /*
+    First it tries the preferred index.
+    If this doesn´t work, it goes through the whole array to find an empty slot.
+    If no empty slot is found, is uses the preferred slot.
+     */
+    private int findEmptySlot(final int startindex) {
+        int slot = startindex;
+        do {
+            if(this.hashset[slot] == null) {
+                return slot;
+            }
+            slot = (slot + 1) % this.hashset.length;
+        } while(this.getByIndex(slot) != null && slot != startindex);
+        return slot;
+    }
+
+    public String toString() {
+        String str = "";
+        for(Element element : this.hashset) {
+            if(element != null) {
+                str += element.toString( ) + ";";
+            }
+        }
+        return str;
+    }
+}
---
+++ aufgaben/src/main/java/e4/simpleHashset/Element.java
@@ -0,0 +1,36 @@
+package e4.simpleHashset;
+
+public class Element {
+
+    private int hash;
+    private int data;
+
+    Element(int hash, int data) {
+        this.hash = hash;
+        this.data = data;
+    }
+
+    public int getHash() {
+        return hash;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if(!(object instanceof Element)) {
+            return false;
+        }
+        return  (((Element) object).getHash() == this.getHash()) && (((Element) object).getData() == this.getData());
+    }
+
+    @Override
+    public String toString() {
+        return "Element [hash=" + hash + ", data=" + data + "]";
+    }
+}
