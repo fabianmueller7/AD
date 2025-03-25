060bb44401b1cc934651a1ed0d98ff06bff41ccb
muellefa <muellerfabian@bluemail.ch>
2025-03-25 18:54:30 +0100

    E6 complete

---
+++ aufgaben/src/main/java/e6/countDownLatch/Turf.java
@@ -0,0 +1,32 @@
+package e6.countDownLatch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CountDownLatch;
+
+public class Turf {
+
+    private static final Logger LOG = LoggerFactory.getLogger(e6.latch.Turf.class);
+    private static final int HORSES = 5;
+
+    /**
+     * Privater Konstruktor.
+     */
+    private Turf() {
+    }
+
+    /**
+     * Main-Demo.
+     * @param args not used.
+     */
+    public static void main(final String[] args) throws InterruptedException {
+        final CountDownLatch starterBox = new CountDownLatch(1);
+        for (int i = 1; i <= HORSES; i++) {
+            Thread.startVirtualThread(new RaceHorse(starterBox, "Horse " + i));
+        }
+        LOG.info("Start...");
+        Thread.sleep(1000);
+        starterBox.countDown();
+    }
+}
